44fc858991cd15d7c1e2b620f1474226bf251211
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-12 15:22:59 +0000

    Dynamic Fields

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [data, setData] = useState([])
+
+  const addMore = () => {
+    setData([...data, {id: data.length+1, name, age}])
+    // setName('')
+    // setAge('')
+  }
+
+  const handleSubmit = () => {
+    console.log(data);
+  }
+
+  const removeRow = (id) => {
+    setData(data => data.filter(row => row.id!=id))
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <table border={1}>
+          <tr>
+            <td><input type="text" name="name" value={name} onChange={e => setName(e.target.value)} /></td>
+            <td><input type="number" name="age" value={age} onChange={e => setAge(e.target.value)} /></td>
+            <td><button>Remove</button></td>
+          </tr>
+          {data.length > 0 && data.map(row => (
+            <tr key={row.id}>
+              <td>{row.name}</td>
+              <td>{row.age}</td>
+              <td><button onClick={() => removeRow(row.id)}>Remove</button></td>
+            </tr>
+          ))}
+        </table>
+        <div>
+          <button onClick={addMore}>Add More</button>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
     </div>
   )
 }
